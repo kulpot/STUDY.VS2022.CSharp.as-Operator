2e805c4d09e2c93a4cc4473adf5ba7d9ab85402c
Kulpot <kulpotwahu@gmail.com>
2023-09-03 02:00:32 +0800

    Add project files.

---
+++ as Operator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=PmMPSHBNi7w&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=32
+//
+
+class Base { }
+class Derived : Base { }
+
+class MainClass
+{
+    static void Main()
+    {
+        var randy = new Random(); 
+        bool randomBool = randy.Next() % 2 == 0;
+        //Base b = new Derived();
+        Base b = randomBool ? new Base() : new Derived();   // error or 1 result
+        //Unhandled exception. System.InvalidCastException: Unable to
+        //cast object of type 'Base' to type 'Derived'.
+
+        
+        //Derived d = b; // error 
+        //Derived d = (Derived)b; // random: if returns fail -- output error exception
+        Derived d = b as Derived;   // random: if Null -- instead of output error exceiption
+        if (d == null)
+            Console.WriteLine("Oh, well that failed.");
+        else
+            Console.WriteLine("WE GOT AN OBJECT!");
+    }
+}
